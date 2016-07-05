bf0463133fd5b801257ff9268d3ab40cf2052089
Gordon Brander <gordonbrander@users.noreply.github.com>
2016-07-05 10:52:27 -0400

    Set overflow boundaries on tooltip

--- main.js
+++ main.js
@@ -3,6 +3,8 @@ const height = 300;
 
 const compose = (a, b) => (x) => a(b(x));
 
+const px = n => n + 'px';
+
 const readX = d => d.x;
 const readY = d => d.y;
 
@@ -19,20 +21,33 @@ const calcY = (data, height, readY) =>
       .range([height, 0])
       .domain(d3.extent(data, readY));
 
-const enter = (container, {width, height}) => {
+const calcTooltipX = (x, width, tooltipWidth) =>
+  (x + (tooltipWidth / 2)) > width ?
+  (width - (tooltipWidth / 2)) :
+  (x - (tooltipWidth / 2)) < 0 ?
+  (tooltipWidth / 2) :
+  x;
+
+const enter = (container, {width, tooltipWidth}) => {
   const xhair = container.append('div')
     .classed('chart-xhair', true);
 
+  const tooltip = container.append('div')
+    .classed('chart-tooltip', true)
+    .style('width', px(tooltipWidth))
+    .style('margin-left', px(-1 * (tooltipWidth / 2)));
+
+  container.append('svg');
+
   container
-    .classed('chart', true);
+    .classed('chart', true)
+    .on('mousemove', function (d) {
+      const x = d3.event.clientX - this.offsetLeft;
+      const tx = calcTooltipX(x, width, tooltipWidth)
 
-  container.append('svg')
-    .on('mousemove', d => {
-      xhair.style('left', d3.event.clientX + 'px');
+      xhair.style('left', px(x));
+      tooltip.style('left', px(tx));
     });
-
-  container.append('div')
-    .classed('chart-tooltip', true);
 
   return container;
 }
@@ -40,6 +55,7 @@ const enter = (container, {width, height}) => {
 // Renders the chart
 const update = (container, series, config) => {
   const {width, height, readX, readY} = config;
+  const graphHeight = height - 100;
 
   const calcD = group => {
     const {data} = group;
@@ -47,14 +63,14 @@ const update = (container, series, config) => {
     const line = d3.line()
       .curve(d3.curveBasis)
       .x(compose(calcX(data, width, readX), readX))
-      .y(compose(calcY(data, height, readY), readY));
+      .y(compose(calcY(data, graphHeight, readY), readY));
 
     return line(data);
   }
 
   container
-    .style('width', width + 'px')
-    .style('height', height + 'px');
+    .style('width', px(width))
+    .style('height', px(height));
 
   const svg = container.selectAll('svg')
     .attr('width', width)
@@ -73,8 +89,7 @@ const update = (container, series, config) => {
   groupEnter
     .append('path')
       .classed('chart-line', true)
-      .style('stroke', group => group.color)
-      .on('mouseover', (d) => log(d));
+      .style('stroke', group => group.color);
 
   const groupAll = group.merge(groupEnter);
 
@@ -89,33 +104,42 @@ const update = (container, series, config) => {
     .append('circle')
     .attr('class', 'chart-dot')
     .attr('cx', compose(calcX(data, width, readX), readX))
-    .attr('cy', compose(calcY(data, height, readY), readY))
-    .attr("r", 3.5)
+    .attr('cy', compose(calcY(data, graphHeight, readY), readY))
+    .attr("r", 2.5)
   .exit()
     .remove();
+
+  const readout = d3.select('.chart-tooltip').selectAll('.chart-tooltip--readout')
+    .data(series)
+
+  const readoutEnter = readout.enter()
+    .append('div')
+    .classed('chart-tooltip--readout', true)
+    .on('mousemove', function (d) {
+      // @TODO hmmm
+      const x = compose(calcX(data, width, readX), readX);
+      this.text(x(d));
+    });
 
   return svg;
 }
 
-const svg = enter(d3.select('#chart'), {
-  width: 800,
-  height: 300
-});
+const svg = enter(d3.select('#chart'), {width: 800, tooltipWidth: 240});
 
 const series = [
   {
-    color: 'blue',
+    color: '#0052b3',
     data: data
   },
   {
-    color: 'red',
+    color: '#00a5ed',
     data: data2
   }
 ]
 
 update(svg, series, {
   width: 800,
-  height: 300,
+  height: 600,
   readX,
   readY
 });
